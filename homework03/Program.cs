2eede65fa4cc7981206308b4f5dcad95ff6cd9ef
Vladimir Korobov <voir703@gmail.com>
2022-12-16 11:52:06 +0500

    add homework 03

---
+++ homework03/Program.cs
@@ -0,0 +1,67 @@
+﻿/*Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+*/
+Console.WriteLine("Введите размер первой матрицы:");
+int[,] a = new int[Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())];
+for (i = 0; i < a.GetLength(0); i++)
+{
+    for (j = 0; j < a.GetLength(1); j++)
+    {
+        Console.Write("a[{i}, {j}] = ", i, j);
+        a[i, j] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Введите размер второй матрицы:");
+int[,] b = new int[Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())];
+for (i = 0; i < b.GetLength(0); i++)
+{
+    for (j = 0; j < a.GetLength(1); j++)
+    {
+        Console.Write("b[{i}, {j}] = ", i, j);
+        a[i, j] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Матрица b:");
+Print(a);
+Console.WriteLine("Матрица b:");
+Print(a);
+  Console.WriteLine("Матрица c = a * b:");
+int[,] c = Multiplication(a, b);
+Print(c);
+/*}
+else
+{
+    int[,] c = new int[m, q];
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < q; j++)
+        {
+            c[i, j] = 0;
+            for (int k = 0; k < n; k++)
+            {
+                c[i, j] += a[i, k] * b[k, j];
+            }
+        }
+    }
+    Console.WriteLine("Произведение двух матриц равно :");
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            Console.Write(c[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+*/
+
+
+
